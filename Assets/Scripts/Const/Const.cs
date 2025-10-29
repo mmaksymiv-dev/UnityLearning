b22548a2f9f1c870ae0f8856b503bcd29bf3ead4
Mykola Maskymiv <kolaymaksimiv@gmail.com>
2025-10-29 16:40:50 +0200

    clear project

--- Assets/Scripts/Const/Const.cs
+++
@@ -1,6 +0,0 @@
-public class Const
-{
-    public const string Horizontal = "Horizontal";
-    public const string Vertical = "Vertical";
-    public const string MouseScrollWheel = "Mouse ScrollWheel";
-}
